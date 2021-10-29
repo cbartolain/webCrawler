f1c50c9ab79408f6f3bef607ce4a2cb405d0d0f5
Christy Bartolain <Christy@nextlevelei.com>
2021-10-28 19:53:26 -0600

    first check-in

---
+++ CoderByteTest/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CoderByteTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var baseUrl = "https://therecount.github.io/interview-materials/project-a";
+            var content = await GetContent(baseUrl + "/1.html");
+
+            //Find any phone numbers and put in list
+            List<string> numbers = new List<string>();
+            numbers.AddRange(FindPhoneNumber(content));
+
+            //Find any links and continue searching
+            var links = GetOtherLinks(content, baseUrl);
+            foreach (var l in links)
+            {
+                content = await GetContent(l.ToString());
+                numbers.AddRange(FindPhoneNumber(content));
+            }
+
+            //Console.WriteLine(content);
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n.ToString());
+            }
+            
+        }
+
+        private static List<string> FindPhoneNumber(string content)
+        {
+            List<string> retval = new List<string>();
+
+            string strRegex = @"([0-9]{3}-[0-9]{3}-[0-9]{4})";
+            Regex matcher = new Regex(strRegex, RegexOptions.None);
+            MatchCollection matches = matcher.Matches(@content);
+
+            foreach (Match m in matches)
+            {
+                retval.Add(m.Value);
+            }
+
+            return retval;
+        }
+
+        private static List<string> GetOtherLinks(string content, string baseUrl)
+        {
+            List<string> Links = new List<string>();
+            string strRegex = @"<a [^>]*href=(?:'(?<href>.*?)')|(?:""(?<href>.*?)"")";
+            Regex matcher = new Regex(strRegex, RegexOptions.None);
+
+            MatchCollection matches = matcher.Matches(@content);
+
+            foreach (Match m in matches)
+            {
+                //Uri testUri = new Uri(m.Value, UriKind.RelativeOrAbsolute);
+                //if (testUri.IsAbsoluteUri)
+                //{
+                //    Links.Add(testUri.ToString());
+                //} else
+                //{
+                //    Links.Add(new Uri((baseUrl, testUri.ToString()).ToString());
+                //}
+                if (m.Value.StartsWith("http"))
+                {
+                    Links.Add(m.Value.Trim('"'));
+                }
+                else
+                {
+                    var baseParts = baseUrl.Split("/");
+                    string urlBuilder = "";
+                    string hrefVal = m.Value.Trim('"').TrimStart('/');
+
+                    urlBuilder += baseParts[0] + "//";
+                    for (var i = 1; i < baseParts.Length; i++)
+                    {
+                        if (!hrefVal.Contains(baseParts[i]))
+                        {
+                            urlBuilder += baseParts[i] + "/";
+                        }
+                    }
+                    urlBuilder += hrefVal;
+
+
+                    Links.Add(urlBuilder);
+                }
+            }
+            return Links;
+        }
+
+        private static async Task<string> GetContent(string url)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent", "C# console program");
+            var retval = await client.GetStringAsync(url);
+
+            return retval;
+
+        }
+    }
+}
